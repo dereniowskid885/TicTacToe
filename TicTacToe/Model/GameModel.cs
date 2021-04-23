b1601c3f8b3bfe818c3422db0c17ae4f447d93a5
Daniel Dereniowski <leinad885@o2.pl>
2021-04-23 13:38:02 +0200

    Layout, MVVM begins

---
+++ TicTacToe/Model/GameModel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using TicTacToe;
+
+namespace TicTacToe.Model
+{
+    class GameModel
+    {
+        // tablica z polami gry
+        public string[] game_fields = new string[9] { " ", " ", " ", " ", " ", " ", " ", " ", " " };
+
+        // true jesli jest tura gracza (X)
+        public bool x_turn = true;
+
+        // true jesli gra sie zakonczyla
+        public bool game_finish;
+
+        // pole tekstowe informujace ktory gracz wykonuje ruch
+        public string player_turn = "Player: X";
+
+        internal string[] newGame()
+        {
+            // wypelnienie pol gry pustym elementem
+            for (var i = 0; i < game_fields.Length; i++)
+                game_fields[i] = " ";
+
+            x_turn = true;
+            return game_fields;
+        }
+
+        internal void changePlayer()
+        {
+            if (x_turn)
+                x_turn = false;
+            else
+                x_turn = true;
+        }
+
+        internal string[] fieldClick(string[] game_fields, bool x_turn, int field)
+        {
+            if (x_turn)
+                game_fields[field] = "X";
+            else
+                game_fields[field] = "O";
+
+            if ((game_fields[0].Equals("X")) & (game_fields[1].Equals("X")) & (game_fields[2].Equals("O")) ^ (game_fields[0].Equals("O")) & (game_fields[1].Equals("O")) & (game_fields[2].Equals("O")))
+                game_finish = true;
+            else if ((game_fields[3].Equals("X")) & (game_fields[4].Equals("X")) & (game_fields[5].Equals("O")) ^ (game_fields[3].Equals("O")) & (game_fields[4].Equals("O")) & (game_fields[5].Equals("O")))
+                game_finish = true;
+            else if ((game_fields[6].Equals("X")) & (game_fields[7].Equals("X")) & (game_fields[8].Equals("O")) ^ (game_fields[6].Equals("O")) & (game_fields[7].Equals("O")) & (game_fields[8].Equals("O")))
+                game_finish = true;
+            else if ((game_fields[0].Equals("X")) & (game_fields[3].Equals("X")) & (game_fields[6].Equals("O")) ^ (game_fields[0].Equals("O")) & (game_fields[3].Equals("O")) & (game_fields[6].Equals("O")))
+                game_finish = true;
+            else if ((game_fields[1].Equals("X")) & (game_fields[4].Equals("X")) & (game_fields[7].Equals("O")) ^ (game_fields[1].Equals("O")) & (game_fields[4].Equals("O")) & (game_fields[7].Equals("O")))
+                game_finish = true;
+            else if ((game_fields[2].Equals("X")) & (game_fields[5].Equals("X")) & (game_fields[8].Equals("O")) ^ (game_fields[2].Equals("O")) & (game_fields[5].Equals("O")) & (game_fields[8].Equals("O")))
+                game_finish = true;
+            else if ((game_fields[0].Equals("X")) & (game_fields[4].Equals("X")) & (game_fields[8].Equals("O")) ^ (game_fields[0].Equals("O")) & (game_fields[4].Equals("O")) & (game_fields[8].Equals("O")))
+                game_finish = true;
+            else if ((game_fields[2].Equals("X")) & (game_fields[4].Equals("X")) & (game_fields[6].Equals("O")) ^ (game_fields[2].Equals("O")) & (game_fields[4].Equals("O")) & (game_fields[6].Equals("O")))
+                game_finish = true;
+
+            changePlayer();
+            return game_fields;
+        }
+
+        internal string playerTurn(string player_turn, bool x_turn)
+        {
+            if(x_turn)
+                return player_turn = "Player: X";
+            else
+                return player_turn = "Player: O";
+        }
+    }
+}
---
+++ TicTacToe/ViewModel/GameViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using System.ComponentModel;
+using TicTacToe.Model;
+
+namespace TicTacToe.ViewModel
+{
+    using Model;
+
+    class GameViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private Model.GameModel game = new Model.GameModel();
+
+        public string[] fields
+        {
+            get { return game.game_fields; }
+            set
+            {
+                game.game_fields = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(fields)));
+            }
+        }
+
+        private ICommand new_game;
+
+        public ICommand newGame
+        {
+            get
+            {
+                return new_game ?? (new_game = new BaseClass.RelayCommand((p) => {
+                    fields = game.newGame();
+                    playerTurn = "Player: X";
+                }, p => true));
+            }
+        }
+
+        public string playerTurn
+        {
+            get { return game.player_turn; }
+            set
+            {
+                game.player_turn = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(playerTurn)));
+            }
+        }
+
+        private ICommand field_click;
+
+        public ICommand fieldClick
+        {
+            get
+            {
+                return field_click ?? (field_click = new BaseClass.RelayCommand((p) => {
+                    fields = game.fieldClick(game.game_fields, game.x_turn, 0);
+                    playerTurn = game.playerTurn(game.player_turn, game.x_turn);
+                }, p => true));
+            }
+        }
+    }
+}
