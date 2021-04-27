8425c48fba45c2d29cd259708017d6e7ca343edd
Daniel Dereniowski <leinad885@o2.pl>
2021-04-27 09:36:45 +0200

    Done

--- TicTacToe/Model/GameModel.cs
+++ TicTacToe/Model/GameModel.cs
@@ -12,11 +12,14 @@ namespace TicTacToe.Model
         // tablica z polami gry
         public string[] game_fields = new string[9] { " ", " ", " ", " ", " ", " ", " ", " ", " " };
 
+        // tablica z wartosciami isEnabled przyciskow na planszy
+        public bool[] game_buttons = new bool[9] { true, true, true, true, true, true, true, true, true };
+
         // true jesli jest tura gracza (X)
         public bool x_turn = true;
 
         // true jesli gra sie zakonczyla
-        public bool game_finish;
+        public bool game_finish = false;
 
         // pole tekstowe informujace ktory gracz wykonuje ruch
         public string player_turn = "Player: X";
@@ -25,44 +28,83 @@ namespace TicTacToe.Model
         {
             // wypelnienie pol gry pustym elementem
             for (var i = 0; i < game_fields.Length; i++)
+            {
                 game_fields[i] = " ";
+                game_buttons[i] = true;
+            }
 
             x_turn = true;
+            game_finish = false;
             return game_fields;
+        }
+
+        internal bool[] buttonEnable()
+        {
+            for (var i = 0; i < game_buttons.Length; i++)
+                game_buttons[i] = true;
+            
+            return game_buttons;
         }
 
         internal void changePlayer()
         {
+            // zmiana tury gracza
             if (x_turn)
                 x_turn = false;
             else
                 x_turn = true;
         }
 
+        internal bool[] buttonClicked(string[] game_fields, bool[] game_buttons, int field)
+        {
+            if ((game_fields[field].Equals("X")) ^ (game_fields[field].Equals("O")))
+                game_buttons[field] = false;
+
+            return game_buttons;
+        }
+
         internal string[] fieldClick(string[] game_fields, bool x_turn, int field)
         {
+            // wypelnienie pola gry w zaleznosci od gracza
             if (x_turn)
                 game_fields[field] = "X";
             else
                 game_fields[field] = "O";
 
-            if ((game_fields[0].Equals("X")) & (game_fields[1].Equals("X")) & (game_fields[2].Equals("O")) ^ (game_fields[0].Equals("O")) & (game_fields[1].Equals("O")) & (game_fields[2].Equals("O")))
+            // sprawdzenie czy ktorys gracz wygral
+            if ((game_fields[0].Equals("X")) & (game_fields[1].Equals("X")) & (game_fields[2].Equals("X")) ^ (game_fields[0].Equals("O")) & (game_fields[1].Equals("O")) & (game_fields[2].Equals("O")))
                 game_finish = true;
-            else if ((game_fields[3].Equals("X")) & (game_fields[4].Equals("X")) & (game_fields[5].Equals("O")) ^ (game_fields[3].Equals("O")) & (game_fields[4].Equals("O")) & (game_fields[5].Equals("O")))
+            else if ((game_fields[3].Equals("X")) & (game_fields[4].Equals("X")) & (game_fields[5].Equals("X")) ^ (game_fields[3].Equals("O")) & (game_fields[4].Equals("O")) & (game_fields[5].Equals("O")))
                 game_finish = true;
-            else if ((game_fields[6].Equals("X")) & (game_fields[7].Equals("X")) & (game_fields[8].Equals("O")) ^ (game_fields[6].Equals("O")) & (game_fields[7].Equals("O")) & (game_fields[8].Equals("O")))
+            else if ((game_fields[6].Equals("X")) & (game_fields[7].Equals("X")) & (game_fields[8].Equals("X")) ^ (game_fields[6].Equals("O")) & (game_fields[7].Equals("O")) & (game_fields[8].Equals("O")))
                 game_finish = true;
-            else if ((game_fields[0].Equals("X")) & (game_fields[3].Equals("X")) & (game_fields[6].Equals("O")) ^ (game_fields[0].Equals("O")) & (game_fields[3].Equals("O")) & (game_fields[6].Equals("O")))
+            else if ((game_fields[0].Equals("X")) & (game_fields[3].Equals("X")) & (game_fields[6].Equals("X")) ^ (game_fields[0].Equals("O")) & (game_fields[3].Equals("O")) & (game_fields[6].Equals("O")))
                 game_finish = true;
-            else if ((game_fields[1].Equals("X")) & (game_fields[4].Equals("X")) & (game_fields[7].Equals("O")) ^ (game_fields[1].Equals("O")) & (game_fields[4].Equals("O")) & (game_fields[7].Equals("O")))
+            else if ((game_fields[1].Equals("X")) & (game_fields[4].Equals("X")) & (game_fields[7].Equals("X")) ^ (game_fields[1].Equals("O")) & (game_fields[4].Equals("O")) & (game_fields[7].Equals("O")))
                 game_finish = true;
-            else if ((game_fields[2].Equals("X")) & (game_fields[5].Equals("X")) & (game_fields[8].Equals("O")) ^ (game_fields[2].Equals("O")) & (game_fields[5].Equals("O")) & (game_fields[8].Equals("O")))
+            else if ((game_fields[2].Equals("X")) & (game_fields[5].Equals("X")) & (game_fields[8].Equals("X")) ^ (game_fields[2].Equals("O")) & (game_fields[5].Equals("O")) & (game_fields[8].Equals("O")))
                 game_finish = true;
-            else if ((game_fields[0].Equals("X")) & (game_fields[4].Equals("X")) & (game_fields[8].Equals("O")) ^ (game_fields[0].Equals("O")) & (game_fields[4].Equals("O")) & (game_fields[8].Equals("O")))
+            else if ((game_fields[0].Equals("X")) & (game_fields[4].Equals("X")) & (game_fields[8].Equals("X")) ^ (game_fields[0].Equals("O")) & (game_fields[4].Equals("O")) & (game_fields[8].Equals("O")))
                 game_finish = true;
-            else if ((game_fields[2].Equals("X")) & (game_fields[4].Equals("X")) & (game_fields[6].Equals("O")) ^ (game_fields[2].Equals("O")) & (game_fields[4].Equals("O")) & (game_fields[6].Equals("O")))
+            else if ((game_fields[2].Equals("X")) & (game_fields[4].Equals("X")) & (game_fields[6].Equals("X")) ^ (game_fields[2].Equals("O")) & (game_fields[4].Equals("O")) & (game_fields[6].Equals("O")))
                 game_finish = true;
+            else if ((game_fields[0].Equals("X") ^ game_fields[0].Equals("O")) & (game_fields[1].Equals("X") ^ game_fields[1].Equals("O")) & (game_fields[2].Equals("X") ^ game_fields[2].Equals("O")) & (game_fields[3].Equals("X") ^ game_fields[3].Equals("O")) & (game_fields[4].Equals("X") ^ game_fields[4].Equals("O")) & (game_fields[5].Equals("X") ^ game_fields[5].Equals("O")) & (game_fields[6].Equals("X") ^ game_fields[6].Equals("O")) & (game_fields[7].Equals("X") ^ game_fields[7].Equals("O")) & (game_fields[8].Equals("X") ^ game_fields[8].Equals("O")))
+            {
+                MessageBox.Show("Round ended as a draw !", "Draw !");
+                return newGame();
+            }
 
+            if ((game_finish) & (x_turn))
+            {
+                MessageBox.Show("Player X won !", "Victory !");
+                return newGame();
+            } else if ((game_finish) & (x_turn == false))
+            {
+                MessageBox.Show("Player O won !", "Victory !");
+                return newGame();
+            }
+
+            // zmiana gracza
             changePlayer();
             return game_fields;
         }
--- TicTacToe/ViewModel/GameViewModel.cs
+++ TicTacToe/ViewModel/GameViewModel.cs
@@ -24,16 +24,13 @@ namespace TicTacToe.ViewModel
             }
         }
 
-        private ICommand new_game;
-
-        public ICommand newGame
+        public bool[] buttonUse
         {
-            get
+            get { return game.game_buttons; }
+            set
             {
-                return new_game ?? (new_game = new BaseClass.RelayCommand((p) => {
-                    fields = game.newGame();
-                    playerTurn = "Player: X";
-                }, p => true));
+                game.game_buttons = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(buttonUse)));
             }
         }
 
@@ -47,17 +44,37 @@ namespace TicTacToe.ViewModel
             }
         }
 
-        private ICommand field_click;
-
-        public ICommand fieldClick
+        private ICommand new_game;
+        public ICommand newGame
         {
             get
             {
-                return field_click ?? (field_click = new BaseClass.RelayCommand((p) => {
-                    fields = game.fieldClick(game.game_fields, game.x_turn, 0);
-                    playerTurn = game.playerTurn(game.player_turn, game.x_turn);
+                return new_game ?? (new_game = new BaseClass.RelayCommand((p) => {
+                    fields = game.newGame();
+                    playerTurn = "Player: X";
+                    buttonUse = game.buttonEnable();
                 }, p => true));
             }
         }
+
+        public ICommand clickEvent(ICommand field_click, int field)
+        {
+            return field_click ?? (field_click = new BaseClass.RelayCommand((p) => {
+                fields = game.fieldClick(game.game_fields, game.x_turn, field);
+                playerTurn = game.playerTurn(game.player_turn, game.x_turn);
+                buttonUse = game.buttonClicked(game.game_fields, game.game_buttons, field);
+            }, p => true));
+        }
+
+        private ICommand field_click_0_0, field_click_0_1, field_click_0_2, field_click_1_0, field_click_1_1, field_click_1_2, field_click_2_0, field_click_2_1, field_click_2_2;
+        public ICommand fieldClick_0_0 { get { return clickEvent(field_click_0_0, 0); } }
+        public ICommand fieldClick_0_1 { get { return clickEvent(field_click_0_1, 3); } }
+        public ICommand fieldClick_0_2 { get { return clickEvent(field_click_0_2, 6); } }
+        public ICommand fieldClick_1_0 { get { return clickEvent(field_click_1_0, 1); } }
+        public ICommand fieldClick_1_1 { get { return clickEvent(field_click_1_1, 4); } }
+        public ICommand fieldClick_1_2 { get { return clickEvent(field_click_1_2, 7); } }
+        public ICommand fieldClick_2_0 { get { return clickEvent(field_click_2_0, 2); } }
+        public ICommand fieldClick_2_1 { get { return clickEvent(field_click_2_1, 5); } }
+        public ICommand fieldClick_2_2 { get { return clickEvent(field_click_2_2, 8); } }
     }
 }
